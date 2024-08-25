d901793b2c2c8bb57fca25b1e8488556464a284b
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2024-08-25 12:52:38 +0500

    updated versel

--- backend/index.js
+++ backend/index.js
@@ -9,19 +9,19 @@ require('dotenv').config();
 
 app.use(cors(
   {
-    // origin:"https://mm-traders-app-frontend.vercel.app",
-    // methods:["POST","GET"],
-    // credentials:true
+    origin:"https://mm-traders-app-frontend.vercel.app",
+    methods:["POST","GET"],
+    credentials:true
   }
 ));
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', 'https://mm-traders-app-frontend.vercel.app'); // Replace '*' with your allowed origin
-//   res.header(
-//       'Access-Control-Allow-Headers',
-//       'Origin, X-Requested-With, Content-Type, Accept',
-//   );
-//   next();
-// })
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://mm-traders-app-frontend.vercel.app'); // Replace '*' with your allowed origin
+  res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+})
 
 app.use(bodyParser.json())
 app.use("/api", userRoutes)
