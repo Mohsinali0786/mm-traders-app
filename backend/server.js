36033198dce0cf593c92ee57090e1b8d3fa366f2
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2024-08-25 15:55:53 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,17 @@
 const { app } = require('.')
 console.log(app, 'app')
-const uri = `mongodb+srv://mohsin00786:mohsin00786@cluster0.9pujbap.mongodb.net/mmGarments?retryWrites=true&w=majority&appName=Cluster0`
+// const uri = `mongodb+srv://mohsin00786:mohsin00786@cluster0.9pujbap.mongodb.net/mmGarments?retryWrites=true&w=majority&appName=Cluster0`
 
-port = 5000
-const mongoose = require('mongoose');
+// port = 5000
+// const mongoose = require('mongoose');
 
-app.listen(port, async () => {
-  // await mongoDB()
-    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then((client) => {
-      console.log('Database Connected')
+// app.listen(port, async () => {
+//   // await mongoDB()
+//     mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then((client) => {
+//       console.log('Database Connected')
 
-    }).catch((err) => {
-      console.log('Err===>', err)
-    });
-  console.log(`Example app listening on port ${port}`)
-})
+//     }).catch((err) => {
+//       console.log('Err===>', err)
+//     });
+  // console.log(`Example app listening on port ${port}`)
+// })
