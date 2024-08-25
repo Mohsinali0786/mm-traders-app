728e58834240d9149a0630c2f2d7d3fed7bb835f
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2024-08-25 14:56:48 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,15 @@
 const { app } = require('.')
-const mongoDB = require('./config/db')
 console.log(app, 'app')
 port = 5000
 const mongoose = require('mongoose');
 
 app.listen(port, async () => {
   // await mongoDB()
-  const mongoDB = async () => {
     mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then((client) => {
       console.log('Database Connected')
 
     }).catch((err) => {
       console.log('Err===>', err)
     });
-  }
-
-
-
   console.log(`Example app listening on port ${port}`)
 })
