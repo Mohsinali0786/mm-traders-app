154b32476004e83ac45da1a653eb906c354f7b8c
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2024-08-25 16:35:29 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,16 +10,6 @@ require('dotenv').config();
 const uri = `mongodb+srv://mohsin00786:mohsin00786@cluster0.9pujbap.mongodb.net/mmGarments?retryWrites=true&w=majority&appName=Cluster0`
 
 
-app.listen(port, async () => {
-  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then((client) => {
-    console.log('Database Connected')
-
-  }).catch((err) => {
-    console.log('Err===>', err)
-  });
-console.log(`Example app listening on port ${port}`)
-})
-
 app.use(cors(
   {
     origin:"https://mm-traders-app-frontend.vercel.app",
@@ -36,8 +26,16 @@ app.use((req, res, next) => {
   next();
 })
 
-
 app.use(bodyParser.json())
 app.use("/api", userRoutes)
 
+app.listen(port, async () => {
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then((client) => {
+    console.log('Database Connected')
+
+  }).catch((err) => {
+    console.log('Err===>', err)
+  });
+console.log(`Example app listening on port ${port}`)
+})
 module.exports ={app}
