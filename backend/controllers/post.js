df9211c004de46ea0bc3c7a26b56bec0890791fd
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2025-01-14 16:59:13 +0500

    Update post.js

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,7 +1,7 @@
 const express = require('express')
 // const router = express.Router()
 const User = require('../models/userSchema')
-const generateToken = require('../config/jwtToken')
+const {generateToken} = require('../config/jwtToken')
 const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs')
 const jsonWebToken = require('jsonwebtoken')
