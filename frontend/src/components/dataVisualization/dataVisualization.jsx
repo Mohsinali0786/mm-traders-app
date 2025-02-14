fa3295a40dd0b7e1d08cce5eb3388456f39b217c
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2025-02-14 19:21:05 +0500

    Update dataVisualization.jsx

--- frontend/src/components/dataVisualization/dataVisualization.jsx
+++ frontend/src/components/dataVisualization/dataVisualization.jsx
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from "react";
 // import PartyDetailModal from "../modal/modal";
 import { Link } from "react-router-dom";
-import "./dataVisualization.css"
+import "./dataVisualization.css";
 export default function DataTable({ data }) {
   console.log(data, "data");
   return (
-    <table cellPadding={30}>
-      <tr>
-        <th>Name</th>
-      </tr>
-      {data &&
-        data.map((data, index) => {
-          return (
-            <tr>
-              <td>{data?.partyName}</td>
-              <td>
-                {/* <PartyDetailModal data={data} /> */}
-                <Link to="/paymentDetail" state={ data }>View Details</Link>
-              </td>
-            </tr>
-          );
-        })}
-    </table>
+    <div className="mainTableContent">
+      <table cellPadding={10}>
+        <tr>
+          <th>Name</th>
+        </tr>
+        {data &&
+          data.map((data, index) => {
+            return (
+              <tr>
+                <td>{data?.partyName}</td>
+                <td>
+                  {/* <PartyDetailModal data={data} /> */}
+                  <Link to="/paymentDetail" state={data}>
+                    View Details
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+      </table>
+    </div>
   );
 }
