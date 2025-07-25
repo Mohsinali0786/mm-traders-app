2a3f94fe84ba17d904d34d6b4d4df57e4cf37075
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2025-07-25 21:12:59 +0500

    Update alertBox.jsx

--- frontend/src/components/alertBox.jsx
+++ frontend/src/components/alertBox.jsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import Alert from '@mui/material/Alert';
 import CheckIcon from '@mui/icons-material/Check';
 
-export default function SimpleAlert({message,status,className}) {
+export default function SimpleAlert({message,status,className,style}) {
   return (
     // <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
-    <Alert className={className} severity={status? status : "success"}>
+    <Alert className={className} severity={status? status : "success"} style={style} sx={{ whiteSpace: 'pre-line' }}>
       {message}
     </Alert>
   );
