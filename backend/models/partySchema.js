547e0fe262ad83800b271e759bfa43b0cadefad7
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2026-07-19 14:11:13 +0500

    Update partySchema.js

--- backend/models/partySchema.js
+++ backend/models/partySchema.js
@@ -1,33 +1,70 @@
-const mongoose =require('mongoose')
-const {Schema}=mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const partySchema=new Schema({
-    // id:{
-    //     type:String,
-    //     required:true
-    // },
-    partyName:{
-        type:String,
-        required:true
-    },
-    date:{
-        type:Date,
-        default:Date.now()
-    },
-    userId:{
-        type:Schema.Types.ObjectId,
-        ref:'user'
-    },
-    hisabKitab:[],
-    type:{
-        type:String,
-        enum : ['SELL','PURCHASER'],
-        // default: 'SELLER'
-    },
-      unitType:{
-        type:String,
-        enum : ['Kg','Metre','Yard'],
-        // default: 'SELLER'
-    }
-})
-module.exports=mongoose.model('party',partySchema)
+// old work
+// const partyDataSchemaModel=new Schema({
+//     // id:{
+//     //     type:String,
+//     //     required:true
+//     // },
+//     partyName:{
+//         type:String,
+//         required:true
+//     },
+//     date:{
+//         type:Date,
+//         default:Date.now()
+//     },
+//     userId:{
+//         type:Schema.Types.ObjectId,
+//         ref:'user'
+//     },
+//     hisabKitab:[],
+//     type:{
+//         type:String,
+//         enum : ['SELL','PURCHASER'],
+//         // default: 'SELLER'
+//     },
+//       unitType:{
+//         type:String,
+//         enum : ['Kg','Metre','Yard'],
+//         // default: 'SELLER'
+//     }
+// })
+
+// new Work
+
+const partyDataSchemaModel = new Schema({
+  // id:{
+  //     type:String,
+  //     required:true
+  // },
+  // partyName:{
+  //     type:String,
+  //     required:true
+  // },
+  // date:{
+  //     type:Date,
+  //     default:Date.now()
+  // },
+  partyId: {
+    type: Schema.Types.ObjectId,
+    ref: "party",
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
+  hisabKitab: [],
+  type: {
+    type: String,
+    enum: ["SELL", "PURCHASER"],
+    // default: 'SELLER'
+  },
+  unitType: {
+    type: String,
+    enum: ["Kg", "Metre", "Yard"],
+    // default: 'SELLER'
+  },
+});
+module.exports = mongoose.model("partyData", partyDataSchemaModel);
