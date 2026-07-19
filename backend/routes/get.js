74db5d919c7b3997b0a07cf73c9fd35683f1d1bc
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2026-07-19 14:32:42 +0500

    Update get.js

--- backend/routes/get.js
+++ backend/routes/get.js
@@ -1,12 +1,14 @@
 const express =require('express')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
-const {getUser, getCurrenciesValue, getHisab, getInwards } = require('../controllers/get');
+const {getUser, getCurrenciesValue, getHisab, getInwards, getAllParties } = require('../controllers/get');
 
 router.get('/getUser', getUser)
 router.get('/getCurrencies', getCurrenciesValue)
 router.get('/gethisab/:id', getHisab)
 router.get('/getInwardEntry/:id', getInwards)
+router.get('/getAllParties/:userId', getAllParties)
+
 
 
 
