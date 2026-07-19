54012cac58a6ab33037cbca6d04e70e473173b6e
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2026-07-19 14:32:58 +0500

    Update getAllUsers.js

--- frontend/src/commonFunctions/getAllUsers.js
+++ frontend/src/commonFunctions/getAllUsers.js
@@ -1,5 +1,23 @@
+const localURL="http://localhost:5000/api/"
+const remoteURL="https://mm-traders-backend-app.vercel.app/api/"
 export async function GetAllUsers(e) {
-    const response = await fetch("https://mm-traders-backend-app.vercel.app/api/getUser", {
+    const response = await fetch(`${remoteURL}getUser`, {
+        method: "GET",
+        headers: {
+            "content-type": "application/json",
+        },
+    });
+    let res = await response.json();
+    if (res && res.success) {
+        // console.log("Get Data from api", res);
+        return res
+    }
+    else {
+        alert('Error')
+    }
+}        
+export async function GetAllParties(userId) {
+    const response = await fetch(`${localURL}getAllParties/${userId}`, {
         method: "GET",
         headers: {
             "content-type": "application/json",
