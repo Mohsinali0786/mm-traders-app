c1147022ca1870225c4e0c2dd4d991459211f4b2
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2026-08-16 15:25:01 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,24 +7,42 @@ const mongoose = require('mongoose');
 const port = 5000
 require('dotenv').config();
 
-const uri = `mongodb+srv://mohsin00786:mohsin00786@cluster0.9pujbap.mongodb.net/mmGarments?retryWrites=true&w=majority&appName=Cluster0`
+// const uri = `mongodb+srv://mohsin00786:mohsin00786@cluster0.9pujbap.mongodb.net/mmGarments?retryWrites=true&w=majority&appName=Cluster0`
 
+// app.use(cors(
+//   {
+//     origin:["https://mm-traders-app-frontend.vercel.app",
+//             "http://localhost:3000",
+//             "http://localhost:5000"
+//           ],
+//     methods:["POST","GET"],
+//     credentials:true
+//   }
+// ));
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5000",
+  "https://mm-traders-app-frontend.vercel.app"
+];
 
-app.use(cors(
-  {
-    origin:"https://mm-traders-app-frontend.vercel.app",
-    methods:["POST","GET"],
-    credentials:true
-  }
-));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://mm-traders-app-frontend.vercel.app'); // Replace '*' with your allowed origin
-  res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  next();
-})
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, origin);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://mm-traders-app-frontend.vercel.app'); // Replace '*' with your allowed origin
+//   res.header(
+//       'Access-Control-Allow-Headers',
+//       'Origin, X-Requested-With, Content-Type, Accept',
+//   );
+//   next();
+// })
 app.get("/", async (req, res) => {
   res.status(200).json({
     message: "Hello developers from GFG",
@@ -33,7 +51,8 @@ app.get("/", async (req, res) => {
 // app.use(bodyParser.json())
 app.use(express.json())
 app.use("/api", userRoutes)
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then((client) => {
+console.log('process.env.MONGO_URI===>', process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then((client) => {
   console.log('Database Connected')
 
 }).catch((err) => {
