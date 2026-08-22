c45d8e2217f18ed32fabf14c2c5395803aa33201
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2026-08-22 15:41:59 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,7 @@ app.use(cors({
     }
   },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 // app.use((req, res, next) => {
