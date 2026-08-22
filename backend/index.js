46b2dbd67f998d136b8bde3405daebd84b0342af
Mohsin Muhammad Ali <mohsinali481997@gmail.com>
2026-08-22 16:02:31 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 }).catch((err) => {
   console.log('Err===>', err)
 });
-
+module.exports = app;
 // app.listen(port,  () => {
 // console.log(`Example app listening on port ${port}`)
 // })
